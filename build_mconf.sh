c43ae64826be61f1fab4dafa7198e32d27a38f2c
Felipe Cecagno <fcecagno@gmail.com>
2011-09-20 13:38:42 -0300

    start using the newest NDK available version, r6b. it decreased the .so sizes it's only available for the mconf build script, the generic one must be updated

--- build_mconf.sh
+++ build_mconf.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-# using the unofficial Crystax NDK
-NDK_DIR=~/Codes/android-ndk-r4-crystax
-PREBUILT=$NDK_DIR/build/prebuilt/linux-x86/arm-eabi-4.4.0
-PLATFORM=$NDK_DIR/build/platforms/android-8/arch-arm
+NDK_DIR=~/Codes/android-ndk-r6b
 
-# if you prefer to use the official Android NDK, uncomment the following commands
-#NDK_DIR=~/Codes/android-ndk-r6b
-#PREBUILT=$NDK_DIR/toolchains/arm-eabi-4.4.0/prebuilt/linux-x86
-#PLATFORM=$NDK_DIR/platforms/android-8/arch-arm
+if [ ! -d "$NDK_DIR" ]; then
+	echo "Please set correctly your Android Native Development Kit path"
+	echo "Current path: $NDK_DIR"
+	return
+fi
 
-#cp $PLATFORM/../../android-3/arch-arm/usr/include/linux/errno.h $PLATFORM/usr/include/linux/
-#cp $PLATFORM/../../android-3/arch-arm/usr/include/linux/posix_types.h $PLATFORM/usr/include/linux/
-#cp $PLATFORM/../../android-3/arch-arm/usr/include/linux/limits.h $PLATFORM/usr/include/linux/
-#cp $PLATFORM/../../android-3/arch-arm/usr/include/linux/stddef.h $PLATFORM/usr/include/linux/
-#cp $PLATFORM/../../android-3/arch-arm/usr/include/linux/fcntl.h $PLATFORM/usr/include/linux/
-#cp $PLATFORM/../../android-3/arch-arm/usr/include/linux/capability.h $PLATFORM/usr/include/linux/
-#cp $PLATFORM/../../android-3/arch-arm/usr/include/linux/stat.h $PLATFORM/usr/include/linux/
-#cp $PLATFORM/../../android-3/arch-arm/usr/include/linux/sockios.h $PLATFORM/usr/include/linux/
-#cp $PLATFORM/../../android-3/arch-arm/usr/include/linux/in.h $PLATFORM/usr/include/linux/
-#cp $PLATFORM/../../android-3/arch-arm/usr/include/linux/in6.h $PLATFORM/usr/include/linux/
+if [ `cat $NDK_DIR/RELEASE.TXT | grep 'r6b' | wc -l` -eq 0 ]; then
+	echo "This script is prepared to compile FFmpeg using the NDK version r6b"
+	echo "Please download the right version of NDK"
+	return
+fi
+
+PREBUILT=$NDK_DIR/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86
+PLATFORM=$NDK_DIR/platforms/android-8/arch-arm
 
 clean() {
 	rm -f jni/*.tmp
@@ -95,8 +91,8 @@ list_files() {
 	do
 		if [ -e $filename ]
 		then
-			mkdir -p `dirname ../ffmpeg_headers/$filename`
-			cp $filename ../ffmpeg_headers/$filename > /dev/null 2>&1
+			mkdir -p `dirname ../built_headers/$filename`
+			cp $filename ../built_headers/$filename > /dev/null 2>&1
 		fi
 	done
 }
@@ -118,12 +114,12 @@ cd jni/ffmpeg
 	--enable-cross-compile \
 	--enable-encoder=flv \
 	--enable-decoder=flv \
-	--cc=$PREBUILT/bin/arm-eabi-gcc \
-	--cross-prefix=$PREBUILT/bin/arm-eabi- \
-	--nm=$PREBUILT/bin/arm-eabi-nm \
+	--cc=$PREBUILT/bin/arm-linux-androideabi-gcc \
+	--cross-prefix=$PREBUILT/bin/arm-linux-androideabi- \
+	--nm=$PREBUILT/bin/arm-linux-androideabi-nm \
 	--extra-cflags="-fPIC -DANDROID -I$PLATFORM/usr/include" \
 	--enable-armv5te \
-	--extra-ldflags="-Wl,-T,$PREBUILT/arm-eabi/lib/ldscripts/armelf.x -Wl,-rpath-link=$PLATFORM/usr/lib -L$PLATFORM/usr/lib -nostdlib $PREBUILT/lib/gcc/arm-eabi/4.4.0/crtbegin.o $PREBUILT/lib/gcc/arm-eabi/4.4.0/crtend.o -lc -lm -ldl" \
+	--extra-ldflags="-Wl,-T,$PREBUILT/arm-linux-androideabi/lib/ldscripts/armelf_linux_eabi.x -Wl,-rpath-link=$PLATFORM/usr/lib -L$PLATFORM/usr/lib -nostdlib $PREBUILT/lib/gcc/arm-linux-androideabi/4.4.3/crtbegin.o $PREBUILT/lib/gcc/arm-linux-androideabi/4.4.3/crtend.o -lc -lm -ldl" \
 	--logfile=../configure.log
 
 rm -rf ../built_headers
